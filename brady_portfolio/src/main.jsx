fca862be42622543d5dd78d2970d9086df5e1c0e
Brady Yang <brady_yang@wistron.com>
2025-04-09 23:10:08 +0800

    add Navbar

--- brady_portfolio/src/main.jsx
+++ brady_portfolio/src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <App />
   </StrictMode>,
 )
