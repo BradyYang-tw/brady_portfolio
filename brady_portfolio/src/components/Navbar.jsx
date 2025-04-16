1822c671aa500ac8cc29e60161b67563686b9543
Brady Yang <brady_yang@wistron.com>
2025-04-16 23:43:22 +0800

    change nav logo

--- brady_portfolio/src/components/Navbar.jsx
+++ brady_portfolio/src/components/Navbar.jsx
@@ -9,7 +9,8 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo" />
+            {/* <img src={logo} alt="logo" /> */}
+            <h1 className="text-3xl font-bold">Brady</h1>
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
             <FaLinkedin/>
