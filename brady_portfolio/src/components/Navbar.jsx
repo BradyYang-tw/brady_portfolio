2a4181507340f5a74bde5eb0a5f8459aae528ccd
Brady Yang <brady_yang@wistron.com>
2025-06-09 23:12:38 +0800

    clean code

--- brady_portfolio/src/components/Navbar.jsx
+++ brady_portfolio/src/components/Navbar.jsx
@@ -9,7 +9,6 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            {/* <img src={logo} alt="logo" /> */}
             <h1 className="text-3xl font-bold">HsinYu</h1>
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
